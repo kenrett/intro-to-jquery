9316654bfc60d2a2e73b7349d39479bffbab8632
Ken Rettberg <kenrettberg@dbc-kenrettberg.local>
2016-11-01 09:47:27 -0700

    ryu can dance

--- hadouken.js
+++ hadouken.js
@@ -4,4 +4,10 @@ $(document).ready(function(){
 
 var makeRyuFight = function(){
   $('#ryu').html("<img src='http://i.imgur.com/90Mmdcm.png'>");
-}
+  $('#ryu img').hover(function(){
+    this.src = 'http://i.imgur.com/nTj3Fxx.gif';
+  },
+  function(){
+    this.src = 'http://i.imgur.com/90Mmdcm.png';
+  });
+};

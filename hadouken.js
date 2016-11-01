ec1e521e51b7bb2f9608e151bf30a4a63783bd68
Ken Rettberg <kenrettberg@dbc-kenrettberg.local>
2016-11-01 10:08:48 -0700

    Ryu can shoot fire balls to the right

--- hadouken.js
+++ hadouken.js
@@ -11,9 +11,16 @@ var makeRyuFight = function(){
     this.src = 'http://i.imgur.com/90Mmdcm.png';
   });
   $('#ryu img').mousedown(function(){
-    this.src = 'http://i.imgur.com/Rfj0a80.png'
+    this.src = 'http://i.imgur.com/Rfj0a80.png';
+    $('#ryu').append("<img class='hadouken' src='http://i.imgur.com/oTyQRvX.gif' style='position:relative; bottom: 103px;'>");
+    $('.hadouken').animate({
+      "margin-left": "50%"
+    },
+      1000, 'swing', function(){
+      this.remove();
+    });
   });
   $('#ryu img').mouseup(function(){
-    this.src = 'http://i.imgur.com/90Mmdcm.png'
+    this.src = 'http://i.imgur.com/90Mmdcm.png';
   });
 };
